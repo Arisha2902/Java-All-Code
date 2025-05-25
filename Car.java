f59d05feccc6fd5aeaab26b47034afddb98060a5
Arisha Firoz <169101584+Arisha2902@users.noreply.github.com>
2025-05-25 16:29:32 +0530

    zz

---
+++ Car.java
@@ -0,0 +1,22 @@
+class Car1{
+    int id;
+
+    //constructor
+    Car1(int id) {
+        this.id = id;
+    }
+
+    void display1(){
+        System.out.println(id+" ");}  
+
+}
+
+public class Car {
+    public static void main(String args[]){   
+     Car1 s1=new Car1(12);   
+     Car1 s2=new Car1(45);  
+     s1.display1();   
+     s2.display1();   
+    }
+    
+}
--- Student3.java
+++ Student3.java
@@ -1,15 +1,29 @@
+//constructor initializting and method with arguments
 class Student2{
   int id ;
   String name ;
-    void display(){System.out.println(id+" "+name);}   
+
+  //constructor
+   Student2(){
+    id= 101;
+    name = "nm";
+   }
+
+  // metgod which accept parameter
+    void display(int id, String name){
+        System.out.println(id+" "+name);}  
+        
+    // method where we will pass value using constructor
+    void display1(){
+        System.out.println(id+" "+name);}  
   
 }
 
 public class Student3 {
     public static void main(String args[]){   
      Student2 s1=new Student2();   
-    Student2 s2=new Student2();   
-     s1.display();   
-   s2.display();   
+     Student2 s2=new Student2();  
+     s1.display1();   
+     s2.display1();   
     }
 }
